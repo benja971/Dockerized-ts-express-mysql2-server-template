887698332f76507a866a2886d96c7d71ca1cc291
Benjamin NIDDAM <nddm.benjamin@gmail.com>
2023-03-01 17:01:51 +0100

    added middlewares

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,24 @@ import express, { Application } from 'express';
 import { join } from 'path';
 import * as dotenv from 'dotenv';
 import { router } from './router';
+import compression from 'compression';
+import fileUpload from 'express-fileupload';
+import cors from 'cors';
+
+console.clear();
 
 dotenv.config({ path: join(__dirname, '..', '.env') });
-
 const app: Application = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
+app.use(compression());
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL,
+	}),
+);
 app.use(router);
 
 app.listen(process.env.PORT, () => {
